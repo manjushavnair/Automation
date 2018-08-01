0083b5a15e47a72c8e58b0e9b1ea5e887726ac04
admin <admin123$>
2018-08-01 08:46:19 +0530

    updated

--- src/com/automation/ui/connected/testcases/clouddataconnection/DataConnectionTest.java
+++ src/com/automation/ui/connected/testcases/clouddataconnection/DataConnectionTest.java
@@ -19,40 +19,18 @@ public class DataConnectionTest extends LoginTest {
     private DataConnectionTest dataconnection_page;
 
     public DataConnectionTest() {
-
-        super();
-        System.out.println("DataConnectionTest called");
-        dataconnection_page = PageFactory.initElements(
-                driver, DataConnectionTest.class);
+         super();
 
     }
 
     /*
      * Test Case 1
-     * 1.Log into  UIAutomation
-     * 2.Navigate to Alert Manager view
-     * 3.Click on Alert Manager
-     * 4. Search for an alert from existing ones
-     * 5. Ensure the alert has been selected and displayed
+     * 1.Log into  Automation
+
      * */
     @Test(priority = 2)
     public void clickAlertManager() {
 
-        try {
-
-            logger.info("Entering into clickAlertManager ");
-
-            Thread.sleep(BASEConstants.WAITTIME100000MILLISEC);
-
-            dataconnection_page.clickAlertManager();
-
-            logger.info("Exiting from clickAlertManager");
-
-        } catch (Exception e) {
-            Assert.fail("Unable to click the  AlertManager Links");
-            e.printStackTrace();
-
-        }
 
     }
 
