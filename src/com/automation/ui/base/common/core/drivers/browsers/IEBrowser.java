2794f250588a44b5323a2cfcfe50aba74819d3fb
admin <admin123$>
2018-08-03 17:13:00 +0530

    updated

--- src/com/automation/ui/base/common/core/drivers/browsers/IEBrowser.java
+++ src/com/automation/ui/base/common/core/drivers/browsers/IEBrowser.java
@@ -3,15 +3,21 @@ package com.automation.ui.base.common.core.drivers.browsers;
 
 import com.automation.ui.base.common.core.UIWebDriver;
 import com.automation.ui.base.common.core.drivers.BrowserAbstract;
+import com.automation.ui.base.common.logging.Log;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.UnexpectedAlertBehaviour;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.CapabilityType;
 
+import java.io.File;
+
 public class IEBrowser extends BrowserAbstract {
 
 
+    private static Logger logger = Logger
+            .getLogger(IEBrowser.class);
 
- private static final String CHROMEDRIVER_PATH_FORMAT = "test/IEDriver/IEDriverServer.exe";
+
 
 
   /*
@@ -21,10 +27,31 @@ public class IEBrowser extends BrowserAbstract {
 
 
    */
+  private static final String CHROMEDRIVER_PATH_WINDOWS = "test/IEDriver/IEDriverServer.exe";
+
 
 
     @Override
     public void setOptions() {
+        String ieBinaryPath = CHROMEDRIVER_PATH_WINDOWS;
+        String osName = System.getProperty("os.name").toUpperCase();
+
+
+
+        File iedriver= new File(ClassLoader.getSystemResource(ieBinaryPath).getPath());
+
+        logger.info("IEDriverPath:"+ ieBinaryPath + " : " +iedriver.getPath());
+
+
+
+        // set application user permissions to 455
+       // iedriver.setExecutable(true);
+
+
+        System.setProperty("webdriver.ie.driver", iedriver.getPath());
+        Log.info("Using ie driver: ", iedriver.getPath());
+
+
     }
 
     @Override
@@ -36,7 +63,7 @@ public class IEBrowser extends BrowserAbstract {
         caps.setCapability("disable-popup-blocking", false);
         caps.setCapability(CapabilityType.UNEXPECTED_ALERT_BEHAVIOUR, UnexpectedAlertBehaviour.ACCEPT);
         //caps.setBrowserName(wdConfig.getBrowserName());
-        return new UIWebDriver(new InternetExplorerDriver(caps), server, false);
+        return new UIWebDriver(new InternetExplorerDriver( caps), server, false);
     }
 
     @Override
--- src/com/automation/ui/connected/testcases/login/Test.java
+++ src/com/automation/ui/connected/testcases/login/Test.java
@@ -2,13 +2,16 @@ package com.automation.ui.connected.testcases.login;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class Test {
 
     public static void main(String[] args) throws InterruptedException {
 //webdriver.firefox.marionette
-        System.setProperty("webdriver.gecko.driver","D:\\Project\\Automation\\target\\classes\\test\\FireFoxDriver\\firefoxdriver_win32\\geckodriver.exe");
-        WebDriver driver = new FirefoxDriver();
+        //  System.setProperty("webdriver.gecko.driver","D:\\Project\\Automation\\target\\classes\\test\\FireFoxDriver\\firefoxdriver_win32\\geckodriver.exe");
+           System.setProperty("webdriver.ie.driver","D:\\Project\\Automation\\target\\classes\\test\\IEDriver\\IEDriverServer.exe");
+
+        WebDriver driver = new InternetExplorerDriver();
         driver.get("http://www.facebook.com");
 
         System.out.println("Successfully opened the website www.facebook.com");
