58de2461d99b121d641f3a052d1a8122f619dbb4
admin <admin123$>
2018-08-07 12:36:43 +0530

    updated for ie

--- src/com/automation/ui/base/common/core/ElementStateHelper.java
+++ src/com/automation/ui/base/common/core/ElementStateHelper.java
@@ -1,5 +1,7 @@
 package com.automation.ui.base.common.core;
 
+import com.automation.ui.connected.pageobjectsfactory.pageobject.base.SiteBasePageObject;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
@@ -12,9 +14,21 @@ public class ElementStateHelper {
 
     public static final int TIMEOUT = 30;
 
+    private static Logger logger = Logger.getLogger(ElementStateHelper.class);
+
     private ElementStateHelper() {
 
     }
+
+
+    public static boolean isElementVisible(WebElement element) {
+    try {
+      return element.isDisplayed();
+    } catch (NoSuchElementException e) {
+      logger.info(e.getMessage());
+      return false;
+    }
+  }
 
     public static boolean isElementVisible(WebElement element, WebDriver webDriver) {
         webDriver.manage().timeouts().implicitlyWait(BASEConstants.WAITTIME500MILLISEC, TimeUnit.MILLISECONDS);
