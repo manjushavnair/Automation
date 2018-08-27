2cb7284eaf3a88f56ff197932a5cf93fa8c34ba4
admin <admin123$>
2018-08-27 18:06:51 +0530

    clint

--- src/com/automation/ui/connected/testcases/dashboardviewtest/UAConnectionDetailsTest.java
+++ src/com/automation/ui/connected/testcases/dashboardviewtest/UAConnectionDetailsTest.java
@@ -1,19 +1,12 @@
-package com.automation.ui.connected.testcases.dashboardviewTest;
+package com.automation.ui.connected.testcases.dashboardviewtest;
 
 /**
  * @author Manjusha Saju
  */
 
-import com.automation.ui.base.common.core.Assertion;
-import com.automation.ui.base.common.prpreaders.AssertDataReader;
 import com.automation.ui.connected.pageobjectsfactory.pageobject.dashboardview.UAConnectionDetails;
-import com.automation.ui.connected.pageobjectsfactory.pageobject.home.HomePage;
-import com.automation.ui.connected.pageobjectsfactory.pageobject.serverdetails.AddServerDetails;
 import com.automation.ui.connected.testcases.base.ConnectedBaseTest;
-import com.automation.ui.connected.testcases.base.ServerType;
 import org.apache.log4j.Logger;
-import org.testng.Assert;
-import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 
