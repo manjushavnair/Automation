6afccacc31c668ec1c00892d9160439c701d61fd
Manjusha Saju <manjushavnair@gmail.com>
2018-08-28 00:03:54 +0530

    dropdown tests

--- src/com/automation/ui/connected/pageobjectsfactory/pageobject/serverdetails/EditServerDetailsCONSTANTS.java
+++ src/com/automation/ui/connected/pageobjectsfactory/pageobject/serverdetails/EditServerDetailsCONSTANTS.java
@@ -2,5 +2,8 @@ package com.automation.ui.connected.pageobjectsfactory.pageobject.serverdetails;
 
 public interface EditServerDetailsCONSTANTS {
 
+    public static String SERVERSAVE =   "//button[@class='primary ng-binding']";
+    public static String SERVERCANCEL ="//button[@class='link ng-binding']";
+
 
 }
--- src/com/automation/ui/connected/pageobjectsfactory/pageobject/serverdetails/tags/TagDetailsCONSTANTS.java
+++ src/com/automation/ui/connected/pageobjectsfactory/pageobject/serverdetails/tags/TagDetailsCONSTANTS.java
@@ -5,4 +5,33 @@ public interface TagDetailsCONSTANTS {
 
     public static String Tag = "//button[@class='ng-binding ng-scope secondaryLink']";
 
+    public static String FILTERTYPE = "//input[@id='s2']";
+
+    public static String NEWTAGFILTER = "//button[@class='primaryLink ng-binding']";
+
+    public static String SAVETAG = "//button[@class='primary ng-binding']";
+
+    public static String CANCELTAG = "//button[@class='link ng-binding']";
+
+    public static String VIEWALLTAG =  "//a[contains(text(),'View all')]";
+
+    public static String SELECTTAG = "//label[@for='st0']//span";
+
+    public static String CLICKONFILTER = "//a[contains(text(),'phd2test1')]";
+
+    public static String DELETETAG = "//a[@class='delete icon']";
+
+
+    public static String TOTALTAGSELECT = "//label[contains(@for,'flt0')]//span";
+
+
+    public static String FILTERNAME = "//input[@id='filterName']";
+
+    public static String SEARCHTYPE = "//input[@placeholder='Type to search']";
+    public static String SAVECHANGES= "//span[contains(@class,'ng-binding ng-scope')]";
+    public static String CANCELFILTER = "//div[contains(@class,'sectionWrapper')]//button[contains(@class,'link ng-binding')][contains(text(),'Cancel')]";
+
+
+
+
 }
