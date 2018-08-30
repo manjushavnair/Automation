b92b814206f863005b134d51028e71d19997edb1
admin <admin123$>
2018-08-30 18:53:52 +0530

    clint

--- runtest.bat
+++ runtest.bat
@@ -15,6 +15,6 @@ echo "pulling code from git"
 
 git pull
 
-mvn -Dmailpass=hari123$  antrun:run@ant-execute clean compile test surefire-report:report
+mvn -Dmailpass=manjusha  antrun:run@ant-execute clean compile test surefire-report:report
 
 @rem mvn -Dmailpass=hari123$  antrun:run@ant-execute clean compile test surefire-report:report postman:send-mail
