382c95a226ca6b7aab1d2a5f77ffa1596c43a7ef
Manjusha Saju <manjushavnair@gmail.com>
2018-08-30 06:45:37 +0530

    dropdown tests

--- src/com/automation/ui/connected/pageobjectsfactory/pageobject/serverdetails/AddServerDetails.java
+++ src/com/automation/ui/connected/pageobjectsfactory/pageobject/serverdetails/AddServerDetails.java
@@ -606,6 +606,32 @@ public class AddServerDetails extends SiteBasePageObject {
             }
 
             Thread.sleep(120000);
+           /*
+           *
+           * <label for="s1" class="ng-binding"><span></span>All tags</label>
+<label for="s2" class="ng-binding"><span></span>Filtered list of tags</label>
+<button class="primaryLink " ng-init="isHower = false">
+                                    Edit Tags
+                                    <ul ch-menu="" class="ch-dropMenu ui-menu ui-widget ui-widget-content" ng-mouseover="isHower = true" ng-mouseleave="isHower = false" id="ui-id-9" role="menu" tabindex="0">
+                                        <li class="ui-menu-item">
+                                            <a class="icon menu ui-menu-item-wrapper" aria-haspopup="true" id="ui-id-10" tabindex="-1" role="menuitem"><span class="ui-menu-icon ui-icon ui-icon-caret-1-e"></span>&nbsp;</a>
+                                            <ul class="ch-dropMenuInner ui-menu ui-widget ui-widget-content ui-front ch-menuClose" ng-class="{'ch-menuOpen': isHower === true, 'ch-menuClose': isHower === false }" role="menu" aria-hidden="true" aria-expanded="false" style="display: none;">
+                                                <li class="exportCodeJQ ui-menu-item">
+                                                    <a class="icon download icon-blue ui-menu-item-wrapper" ng-click="steps.ExportConfigFile()" id="ui-id-11" tabindex="-1" role="menuitem">Export Config</a>
+                                                </li>
+                                                <li class="importCodeJQ ui-menu-item">
+                                                    <a class="icon upload icon-blue ui-menu-item-wrapper" id="ui-id-12" tabindex="-1" role="menuitem">Import Config</a>
+                                                </li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </button>
+
+                                <button class="primary next ng-binding" ng-click="steps.nextClick()" ng-mousedown="steps.nextMousedown()">Next</button>
+
+                                <a class="cloud icon" alt="CloudDataMenu">Cloud Data</a>
+                                <button class="link back ng-binding" ng-click="steps.backClick()">Back</button>
+           * */
 
 
         } catch (Exception e) {
