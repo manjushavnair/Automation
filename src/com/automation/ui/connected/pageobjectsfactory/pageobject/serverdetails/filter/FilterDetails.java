2b7c7ddc6ac55bac7d11a45a4674616302c99e9a
Manjusha Saju <manjushavnair@gmail.com>
2018-09-01 15:35:52 +0530

    dropdown tests

--- src/com/automation/ui/connected/pageobjectsfactory/pageobject/serverdetails/filter/FilterDetails.java
+++ src/com/automation/ui/connected/pageobjectsfactory/pageobject/serverdetails/filter/FilterDetails.java
@@ -8,6 +8,7 @@ import org.testng.Reporter;
 
 public class FilterDetails extends SiteBasePageObject {
 
+
     private static Logger logger = Logger.getLogger(FilterDetails.class);
     @FindBy(xpath = FilterDetailsCONSTANTS.ALLTAG)
     private WebElement allTag;
