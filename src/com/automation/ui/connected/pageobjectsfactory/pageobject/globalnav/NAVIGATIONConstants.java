7a823defcd6566b7354c83c8b26e046691e301b9
Manjusha Saju <manjushavnair@gmail.com>
2018-09-09 12:37:15 +0530

    dropdown tests

--- src/com/automation/ui/connected/pageobjectsfactory/pageobject/globalnav/NAVIGATIONConstants.java
+++ src/com/automation/ui/connected/pageobjectsfactory/pageobject/globalnav/NAVIGATIONConstants.java
@@ -6,13 +6,12 @@ public interface NAVIGATIONConstants {
 
     public static String CLOUDAVATAR = "//img[contains(@ng-src,'content/images/useravatar.png')]";
 
-    public static String LOGOUTBUTTON = "//a[contains(text(),'Logout')]";
+   // public static String LOGOUTBUTTON = "//a[contains(text(),'Logout')]";
 
-    // public String FILTER_VIEWALL = "//a[@class='cloud icon']";
+    public static String LOGOUTBUTTON = "//a[@class='logout icon']";
  // public String FILTER_VIEWALL = "//a[@class='cloud icon']";
 
 
-    //a[contains(text(),'Logout')]
 
 
 }
