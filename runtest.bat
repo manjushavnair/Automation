912dc5f7cfc0cfabd3954ad286b1decefe2813b6
admin <admin123$>
2018-09-15 17:12:05 +0530

    clint

--- runtest.bat
+++ runtest.bat
@@ -14,8 +14,13 @@ taskkill /IM java /F
 echo "pulling code from git"
 
 git pull
+#T 2 for use 2 cores of CPU
+#dependencyLocationsEnabled all external denendencies are ignored
+#Dmailpass password of smtp to sent mail
+#ant execute target will create directories while build
+#report will create the test results
 
-mvn -T 4   -DdependencyLocationsEnabled=false -Dmailpass=manjusha  antrun:run@ant-execute clean compile test surefire-report:report
+mvn -T 2   -DdependencyLocationsEnabled=false -Dmailpass=manjusha  antrun:run@ant-execute clean compile test surefire-report:report
 
 
 @rem mvn -Dmailpass=hari123$  antrun:run@ant-execute clean compile test surefire-report:report postman:send-mail
